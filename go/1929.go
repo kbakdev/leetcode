197e6bc873f07612e79bc813cf63326c8388601f
Kacper Bąk <kacper.bak@sonderfin.com>
2022-12-02 22:27:38 +0100

    1929

--- go/1929.go
+++ go/1929.go
@@ -1,8 +1,5 @@
 package _go
 
 func getConcatenation(nums []int) []int {
-	ans := make([]int, 0, len(nums)*2)
-	ans = append(ans, nums...)
-	ans = append(ans, nums...)
-	return ans
+	return append(nums, nums...)
 }
